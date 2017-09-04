fa55442ae56597655c931aa07afc06a11f7d69d9
adas <adas@XIPL0617>
2017-09-04 20:08:19 +0530

    Solution created

---
+++ Codebase/xpPortal/xpPortal/BL/BusinessAccessLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xpPortal.BL
+{
+    public class BusinessAccessLayer
+    {
+    }
+}
---
+++ Codebase/xpPortal/xpPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xpPortal.MvcApplication" Language="C#" %>
