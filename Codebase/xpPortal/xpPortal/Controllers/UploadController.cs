a68088a4a4fd6c14c6335895c294108ff2ae17a4
Sagar Pawar <spawar@legalzoom.com>
2017-09-09 00:44:21 +0530

    1.Added Upload controller and its html 2.Added new dashboard for Applicant after login screen. 3. Added link to go to upload Document screen. 4.Added JS folders and all files in content folder.

---
+++ Codebase/xpPortal/xpPortal/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using xpPortal.BL;
+using xpPortal.Models;
+
+namespace xpPortal.Controllers
+{
+    //[Authorize]
+    public class UploadController : Controller
+    {
+       
+        
+        public UploadController()
+        {
+
+        }
+
+        // GET: Login
+        public ActionResult UploadDocument()
+        {
+            return View("UploadDocument");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Login(LoginViewModel model)
+        {
+            BusinessLayer blObject = new BusinessLayer();
+            bool isValiduser=blObject.ValidateUser(model);
+
+            if(isValiduser)
+                return RedirectToAction("Index","Home"); 
+            else
+                ModelState.AddModelError("", "Invalid username or password.");
+            
+            return View("Index");
+        }
+    }
+
+
+}
