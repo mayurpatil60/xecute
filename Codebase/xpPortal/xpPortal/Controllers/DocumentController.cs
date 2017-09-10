d6d570c6300b1fca633d257da80c08ec7e67a5a3
Sagar Pawar <spawar@legalzoom.com>
2017-09-10 23:18:53 +0530

    Upload Files to Server folder

--- Codebase/xpPortal/xpPortal/Controllers/DocumentController.cs
+++ Codebase/xpPortal/xpPortal/Controllers/DocumentController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -25,22 +28,65 @@ namespace xpPortal.Controllers
             return View("UploadDocument");
         }
 
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //[ValidateInput(false)]
-        //public ActionResult Login(LoginViewModel model)
-        //{
-        //    BusinessLayer blObject = new BusinessLayer();
-        //    bool isValiduser=blObject.ValidateUser(model);
 
-        //    if(isValiduser)
-        //        return RedirectToAction("Index","Home"); 
-        //    else
-        //        ModelState.AddModelError("", "Invalid username or password.");
-            
-        //    return View("Index");
-        //}
+        [HttpPost]
+        public ActionResult UploadDocument(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedDocuments"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public JsonResult UploadHomeReport()
+        {
+            try
+            {///http://www.c-sharpcorner.com/uploadfile/manas1/upload-files-through-jquery-ajax-in-asp-net-mvc/
+                //  Get all files from Request object  
+                HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                      
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+
+                        // Get the complete folder path and store the file inside it.  
+                        fname = Path.Combine(Server.MapPath("~/UploadedDocuments/"), fname);
+                        file.SaveAs(fname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json("1");                               
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("0");
+            }           
+        }
     }
 
 
